a8d232ee4e64cd798fb79ae24e04094d22474e18
toru <sting@nifty.com>
2019-10-03 13:01:54 +0900

    migrationファイルがおかしくなっていたので、再生成。

--- db/migrate/20191002080435_create_phases.rb
+++
@@ -1,9 +0,0 @@
-class CreatePhases < ActiveRecord::Migration[5.2]
-  def change
-    create_table :phases do |t|
-      t.string :name, null: false
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_02_102538) do
+ActiveRecord::Schema.define(version: 2019_10_03_030653) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -109,7 +109,6 @@ ActiveRecord::Schema.define(version: 2019_10_02_102538) do
     t.text "introduction"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
