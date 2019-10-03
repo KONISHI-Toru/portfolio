2ac835d159ff8482608009f94efc3ed83777e7ec
toru <sting@nifty.com>
2019-10-03 14:05:45 +0900

    文法ミスを修正。

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,9 +3,9 @@ class Project < ApplicationRecord
   has_one_attached :hw_diagram
   has_one_attached :sw_diagram
   has_many :project_tech_tags
-  has_many :tech_tags, through: :project_tech_tags,
+  has_many :tech_tags, through: :project_tech_tags
   has_many :project_phases
-  has_many :phases, through: :project_phases,
+  has_many :phases, through: :project_phases
   has_many :project_roles
-  has_many :roles, through: :project_roles,
+  has_many :roles, through: :project_roles
 end
