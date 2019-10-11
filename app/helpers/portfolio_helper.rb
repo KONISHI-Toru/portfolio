129338cce89226915164cec5c1058701d0352788
toru <sting@nifty.com>
2019-10-11 13:06:34 +0900

    表示処理の一部をhelper化。

--- app/helpers/portfolio_helper.rb
+++ app/helpers/portfolio_helper.rb
@@ -1,2 +1,44 @@
+# coding: utf-8
 module PortfolioHelper
+  # 文字列を数値文字参照に変換する。
+  def numerical_character_references(str)
+    if str.nil?
+      return ""
+    end
+
+    str.bytes.map{|v| "&\#x#{v.to_s(16)};"}.join
+  end
+
+  # Unicode数値参照で偽装したmailtoリンクを生成する。
+  def mailto_link(user)
+    if user.nil?
+      return ""
+    end
+
+    mailto = numerical_character_references("mailto:")
+    converted = numerical_character_references(user.email)
+    raw "<a href=\"#{mailto}#{converted}\">#{converted}</a>"
+  end
+
+  # プロジェクトの期間を表示するためのヘルパー。
+  def project_period(project)
+    if project.nil?
+      return ""
+    end
+
+    from = ""
+    if project.from.present?
+      from = project.from.strftime('%Y年%-m月')
+    end
+    to = ""
+    if project.to.present?
+      to = project.to.strftime('%Y年%-m月')
+    end
+
+    if from == "" && to == ""
+      return ""
+    end
+
+    "#{from} 〜 #{to}"
+  end
 end
---
+++ test/helpers/portfolio_helper_test.rb
@@ -0,0 +1,47 @@
+# coding: utf-8
+class PortfolioHelperTest < ActionView::TestCase
+  test "Return Unicode numeric character references" do
+    str = "mailto:"
+
+    assert_equal "&#x6d;&#x61;&#x69;&#x6c;&#x74;&#x6f;&#x3a;",
+                  numerical_character_references(str)
+  end
+
+  test "数値文字参照で隠蔽されたmailtoリンクを返す。" do
+    user = User.new
+    user.email = "sting@nifty.com"
+
+    assert_equal "<a href=\"&#x6d;&#x61;&#x69;&#x6c;&#x74;&#x6f;&#x3a;&#x73;&#x74;&#x69;&#x6e;&#x67;&#x40;&#x6e;&#x69;&#x66;&#x74;&#x79;&#x2e;&#x63;&#x6f;&#x6d;\">&#x73;&#x74;&#x69;&#x6e;&#x67;&#x40;&#x6e;&#x69;&#x66;&#x74;&#x79;&#x2e;&#x63;&#x6f;&#x6d;</a>",
+                  mailto_link(user)
+  end
+ 
+  test "プロジェクトの期間文字列を返す。" do
+    project = Project.new
+    project.from = DateTime.new(2019, 2, 1, 12, 30, 45)
+    project.to = DateTime.new(2019, 4, 10, 12, 30, 45)
+
+    assert_equal "2019年2月 〜 2019年4月",
+                 project_period(project)
+  end
+
+  test "開始のみのプロジェクト期間文字列を返す。" do
+    project = Project.new
+    project.from = DateTime.new(2019, 2, 1, 12, 30, 45)
+
+    assert_equal "2019年2月 〜 ",
+                 project_period(project)
+  end
+
+  test "終了のみのプロジェクト期間文字列を返す。" do
+    project = Project.new
+    project.to = DateTime.new(2019, 4, 10, 12, 30, 45)
+
+    assert_equal " 〜 2019年4月",
+                 project_period(project)
+  end
+
+  test "引数がnilの場合、空文字列を返す。" do
+    assert_equal "",
+                 project_period(nil)
+  end
+end
