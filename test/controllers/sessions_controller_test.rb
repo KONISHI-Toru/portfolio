2199574e7ad6ec1d7c46205c795f47768a812f37
toru <sting@nifty.com>
2019-10-18 18:37:03 +0900

    SessionControllerのテスト内容見直し。

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,19 +1,31 @@
+# coding: utf-8
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get sessions_new_url
+  test "ログイン画面が返されること" do
+    get login_url
     assert_response :success
   end
 
-  test "should get create" do
-    get sessions_create_url
-    assert_response :success
+  test "ログインできること" do
+    post login_url, params: { session: { email: "test1@test.com", password: "testpass1" } }
+    assert_response :redirect
+    assert_redirected_to root_url
+    assert_equal session[:user], users(:test1)
   end
 
-  test "should get destroy" do
-    get sessions_destroy_url
+  test "ログインに失敗すること" do
+    post login_url, params: { session: { email: "test1@test.com", password: "xxx" } }
     assert_response :success
+    assert session[:user].nil?
   end
+  
+  test "ログアウトしたらログイン画面にリダイレクトされること" do
+    post login_url, params: { session: { email: "test1@test.com", password: "testpass1" } }
 
+    delete logout_url
+    assert_response :redirect
+    assert_redirected_to login_url
+    assert session[:user].nil?
+  end
 end
