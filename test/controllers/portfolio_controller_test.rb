4d8f17542af9934c82ff5119f222d2dbd1323eea
toru <sting@nifty.com>
2019-10-29 12:10:31 +0900

    アプリケーション名を修正。

--- test/controllers/portfolio_controller_test.rb
+++ test/controllers/portfolio_controller_test.rb
@@ -1,14 +1,17 @@
+# coding: utf-8
 require 'test_helper'
 
 class PortfolioControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get portfolio_index_url
     assert_response :success
+    assert_select "title", "ポートフォリオ"
   end
 
   test "should get show" do
     get portfolio_show_url, params: { portfolio_form: { user_id: users(:test1).id } }
     assert_response :success
+    assert_select "title", "ポートフォリオ"
   end
 
 end
