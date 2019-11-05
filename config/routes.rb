59481d2808bea9ea7042dd1ef8cf4e9f95ec43ee
toru <sting@nifty.com>
2019-11-05 17:46:44 +0900

    Layout/BlockAlignment 修正。

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,15 @@ Rails.application.routes.draw do
   post   'login',  to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   namespace :admin do
-      resources :users
-      resources :projects
-      resources :phases
-      resources :roles
-      resources :tech_categories
-      resources :tech_tags
+    resources :users
+    resources :projects
+    resources :phases
+    resources :roles
+    resources :tech_categories
+    resources :tech_tags
 
-      root to: "projects#index"
-    end
+    root to: "projects#index"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "portfolio#index"
