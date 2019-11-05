d6f1193ca7d827fa48211b08fc81d59938ac8edd
toru <sting@nifty.com>
2019-11-05 16:21:11 +0900

    rubocop-rails をインストール。

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,19 +1,15 @@
 module SessionsHelper
 
   def log_in(user)
-    #session[:user_id] = user.id
     session[:user] = user
-
-    @current_user = user
   end
 
   def log_out
     session.delete(:user)
-    @current_user = nil
   end
 
   def current_user
-    @current_user = session[:user]
+    session[:user]
   end
 
   def logged_in?
