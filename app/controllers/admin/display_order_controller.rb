2d5b6a1b2bf1a5c71ada772e0f7adf099694fd74
toru <sting@nifty.com>
2019-11-06 17:30:32 +0900

    Style/TrailingCommaInArguments 修正。

--- app/controllers/admin/display_order_controller.rb
+++ app/controllers/admin/display_order_controller.rb
@@ -3,7 +3,7 @@ module Admin
     def order
       @order ||= Administrate::Order.new(
         params.fetch(resource_name, {}).fetch(:order, 'display_order'),
-        params.fetch(resource_name, {}).fetch(:direction, 'asc'),
+        params.fetch(resource_name, {}).fetch(:direction, 'asc')
       )
     end
   end
