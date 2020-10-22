835e97c52a0fa3b7e8df35b249e621c1b93c8781
KONISHI Toru <sting@nifty.com>
2020-10-22 14:36:01 +0900

    Administrate を Rails6(Webpacker) に対応。

---
+++ app/javascript/packs/administrate.js
@@ -0,0 +1,19 @@
+require("@rails/ujs").start()
+//require("turbolinks").start()
+
+// The next line you only need if you want ActiveStorage support
+require("@rails/activestorage").start()
+
+require("bootstrap");
+
+// The next line you only need if you need channels in administrate
+//require("channels")
+
+// The next two lines you only need if you want ActionText support
+//require("trix")
+//require("@rails/actiontext")
+
+require("../javascripts/administrate")
+
+//import '../stylesheets/administrate/application.scss'
+//import '../stylesheets/administrate.scss'
